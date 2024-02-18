96e15b3c9d16b418028f674ca530a9282c3593a0
MateusDebut <mateusdossantosribeiro100@gmail.com>
2024-02-18 12:31:08 -0300

    loop

---
+++ Listas/Lista2/Exercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int contador = 0; contador <= 100000; contador++)
+        {
+            Console.WriteLine("Isso é um loop");
+        }
+    }
+}
