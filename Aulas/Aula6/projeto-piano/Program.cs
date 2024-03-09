d97b4b86bd32b9b4e0289bea567507bb38d7b5e6
MateusDebut <mateusdossantosribeiro100@gmail.com>
2024-03-09 02:19:30 -0300

    tabuleiro

---
+++ Aulas/Aula6/projeto-piano/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program 
+{
+    static void Main(string[] args)
+    {
+        int contador = 7;
+
+        while (contador >= 1)
+        {
+            if (contador % 2 == 0)
+            {
+                Piano.ReproduzTecla(4, contador);
+            }
+            contador--;
+        }
+    }
+}
